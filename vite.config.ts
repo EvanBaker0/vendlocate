c00892299888fd6463e68d4e60b99cd69fe9d547
Evan <evanbaker127@gmail.com>
2026-06-01 23:41:52 -0500

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
+  base: process.env.GH_PAGES ? '/vendlocate/' : '/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
